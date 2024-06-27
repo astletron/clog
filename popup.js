b3867cb80fcd02863a55318e8d9ece55f2a47cf7
generic <mike@crushyard.com>
2024-06-26 21:10:49 -0400

    initial

---
+++ popup.js
@@ -0,0 +1,4 @@
+console.log('This is a popup!');
+console.log(document);
+document.getElementById("community-fake-count").innerHTML = Math.floor( Math.random() * 2048 );
+document.getElementById("community-real-count").innerHTML = Math.floor( Math.random() * 2048 );
